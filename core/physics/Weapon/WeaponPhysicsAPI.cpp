646820a3ee9cbccd97720b152954f0c4f79b84dc
DumbestPerson224 <eTmullall@outlook.com>
2025-03-04 10:22:19 -0500

    Update WeaponPhysicsAPI.cpp

--- core/physics/Weapon/WeaponPhysicsAPI.cpp
+++ core/physics/Weapon/WeaponPhysicsAPI.cpp
@@ -1,17 +1,17 @@
 #include "WeaponPhysics.h";
 using namespace WeaponEngine;
 WeaponEngine::WeaponPhysics::WeaponPhysics() : weaponDeceleration(0), weaponMass(0), weaponVelocity(0), weaponAcceleration(0), weaponMomentum(0), weaponForce(0),  weaponDensity(0) {};
-// Calculates the acceleration of the weapon
+// Calculates the acceleration of a weapon
 void WeaponEngine::WeaponPhysics::calculateWeaponAcceleration(float initialVelocity, float finalVelocity, float time) {
     WeaponEngine::WeaponPhysics::weaponAcceleration = (finalVelocity - initialVelocity) / time;
 }
 
-// Calculates the velocity of the weapon
+// Calculates the velocity of a weapon
 void WeaponEngine::WeaponPhysics::calculateWeaponVelocity(float initialVelocity, float time) {
     WeaponEngine::WeaponPhysics::weaponVelocity = initialVelocity + (WeaponEngine::WeaponPhysics::weaponAcceleration * time);
 }
 
-// Calculates the deceleration of the weapon
+// Calculates the deceleration of a weapon
 void WeaponEngine::WeaponPhysics::calculateWeaponDeceleration(float initialVelocity, float finalVelocity, float time) {
     WeaponEngine::WeaponPhysics::weaponDeceleration = (finalVelocity - initialVelocity) / time;
 }
