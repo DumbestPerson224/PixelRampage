148768f4bbd32790a4bb05f4f660f0666d61334a
DumbestPerson224 <eTmullall@outlook.com>
2025-03-22 14:44:13 -0400

    Delete auth.php

--- auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-$headers = getallheaders();
-header("Content-Type: application/text");
-if(isset($headers["username"]) && isset($headers["password"])){
-    $username = strip_tags(filter_input($headers["username"], FILTER_SANITIZE_STRING));
-    $password = strip_tags(filter_input($headers["password"], FILTER_SANITIZE_STRING));
-}else if(isset($headers["session"])){
-    header("session: true");
-}
