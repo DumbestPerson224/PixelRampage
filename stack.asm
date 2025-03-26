b55c6c746105b5e1c8b7731eaa9af019da702122
DumbestPerson224 <eTmullall@outlook.com>
2025-03-26 11:30:53 -0400

    Added stack management protection features

---
+++ stack.asm
@@ -0,0 +1,18 @@
+section .text
+  global _checkStack 
+  
+_checkStack:
+	push ebp 
+	mov ebp, esp 
+	lea edx, [ebp + 8]
+	cmp esp, edx 
+	jne error 
+
+return:
+	pop ebp 
+	ret 
+	
+error:
+	pop ebp 
+	mov eax, -1 
+	ret 
